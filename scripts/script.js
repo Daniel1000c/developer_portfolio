fd9c96ef2f2fd535a8d5242d4dc8a74f08f60920
Daniel Velez <velezdaniel516@gmail.com>
2024-12-31 10:31:01 -0800

    added scroll to top function to script.js and deleted progress bar fill function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,22 @@
-//Create fill animation for progress bar
-document.addEventListener("DOMContentLoaded", () => {
-    const progressBars = document.querySelectorAll(".progressFill");
-    
-    progressBars.forEach(bar => {
-        const targetPercentage = bar.getAttribute("data-percentage").replace("%", "");
-        bar.style.setProperty("--target-width", `${targetPercentage}%`);
-    });
-});
+//Create scroll to top function
+
+//Get the button id
+let myBtn = document.getElementById("myBtn");
+
+//When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+//Create scroll function
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myBtn.style.display = "block";
+    } else {
+        myBtn.style.display = "none";
+    }
+}
+
+//When the user clicks on the button, scroll to the top of the document
+function topFunction () {
+    document.body.scrollTop = 0;// For safari browsers
+    document.documentElement.scrollTop = 0; // For chrome, firefox, IE and Opera browsers
+}

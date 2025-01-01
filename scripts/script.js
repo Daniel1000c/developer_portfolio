46efcf80768818a9b3e2a867071bf48ea510b0a2
Daniel Velez <velezdaniel516@gmail.com>
2025-01-01 13:32:44 -0500

    added menu function and click out of menu function

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,38 @@ function topFunction () {
     document.body.scrollTop = 0;// For safari browsers
     document.documentElement.scrollTop = 0; // For chrome, firefox, IE and Opera browsers
 }
+
+//Create nav menu toggle function
+
+
+//Get menu elements
+const menuIcon = document.getElementById("menuIcon");
+const navMenu = document.getElementById("navMenu");
+
+//Toggle the menu and change the menu icon
+menuIcon.addEventListener("click", (e) => {
+    navMenu.classList.toggle("active");
+
+    //Change the menu icon to x based on click
+    if(navMenu.classList.contains("active")) {
+        menuIcon.classList.replace("bx-menu", "bx-x");
+    } else {
+        menuIcon.classList.replace("bx-x", "bx-menu");
+    }
+});
+
+//Close menu if user clicks outside of it
+document.addEventListener("click", (e) => {
+   if(e.target.contains(e.target) && !menuIcon.contains(e.target)) {
+    //Close the nav menu
+    navMenu.classList.remove("active");
+
+    //Change the menu icon to x based on click
+    menuIcon.classList.replace("bx-x", "bx-menu");
+   } 
+});
+
+//Prevent the menu from closing when user clicks inside of it
+navMenu.addEventListener("click", (e) => {
+    e.stopPropagation();
+});

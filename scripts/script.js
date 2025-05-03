4a839fe193a65f067dce9676a33bb864cf84a86d
Daniel Velez <velezdaniel516@gmail.com>
2025-05-03 13:44:16 -0400

    add home link to nav bar, add ms auto to bar orientation, and add span to navbar brand

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,14 @@ const header = `
     <div class="container">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.html">Daniel Velez</a>
+                <a class="navbar-brand" href="index.html"><span>Daniel</span> Velez</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
